05c6bf9e8a285de7b37686b848cd98253e836748
Brubel <brubel@ucsd.edu>
2021-11-11 11:21:51 -0800

    finsihed(again)

--- Mission_to_mars/app.py
+++ Mission_to_mars/app.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import pymongo
 
 # set up
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.mars_db
+# conn = 'mongodb://localhost:27017'
+# client = pymongo.MongoClient(conn)
+# db = client.mars_db
     
-# store this result in Mongo as python dictionary
+
+#db.mars_data.insert(scrape_())
+
 @app.route("/scrape")
 def scrape_():
     from scrape_mars import scrape
-    return scrape()
+    mars = scrape()
 
-db.mars_data.insert(scrape_())
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_db.update({}, mars, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
 
 @app.route("/")
 def query():
-    mars = mongo.db.mars_data.find_one()
-    
+    mars = mongo.db.mars_db.find_one()
     # Return the template with the teams list passed in
     return render_template('index.html', data = mars)
 
